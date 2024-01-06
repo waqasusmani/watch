de0d3860ddd2376bb32c667db60fe5aa408ed59a
Waqas Usmani <waqas-usmani@hotmail.com>
2024-01-06 13:06:50 +0500

    tick-sound

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var hand = document.getElementById("hand")
 var rotationDegree = 0
 var ticksCounter = document.getElementById("ticks")
 var ticks = 0
+var tickSoundElem = document.getElementById("tick-sound")
 setInterval(()=>{
     if (rotationDegree == 360) {
         rotationDegree = 0
@@ -11,4 +12,5 @@ setInterval(()=>{
     ticks+=1
     ticksCounter.innerText = "Ticks: "+ticks
     console.log("Rotation Degree: " + rotationDegree)
+    tickSoundElem.play()
 },1000)
